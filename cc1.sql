88340c38c26cf66615d0505e3f7e996d12740af8
mazurekevin <71667319+mazurekevin@users.noreply.github.com>
2021-11-21 22:19:20 +0100

    Add files via upload

---
+++ cc1.sql
@@ -0,0 +1,77 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.5
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3307
+-- Generation Time: Nov 21, 2021 at 09:18 PM
+-- Server version: 5.7.24
+-- PHP Version: 7.4.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `cc1`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `service`
+--
+
+CREATE TABLE `service` (
+  `Email` varchar(255) NOT NULL,
+  `Name` varchar(255) NOT NULL,
+  `Price` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `service`
+--
+
+INSERT INTO `service` (`Email`, `Name`, `Price`) VALUES
+('test@test.fr', 'Service 1', 50),
+('test@test.fr', 'Service 1', 50),
+('test@test.fr', 'Service 1', 50),
+('test@test.fr', 'Service 1', 50),
+('Jetest@test.fr', 'Service 2', 50),
+('test@test.fr', 'Service 2', 100),
+('jetest@test.fr', 'Service 2', 100),
+('mail@test.fr', 'Service 1', 50);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `Firstname` varchar(255) NOT NULL,
+  `Lastname` varchar(255) NOT NULL,
+  `Age` int(100) NOT NULL,
+  `Email` varchar(255) NOT NULL,
+  `Password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`Firstname`, `Lastname`, `Age`, `Email`, `Password`) VALUES
+('Kevin', 'MAZURE', 21, 'test@test.fr', 'jesuislemdp'),
+('Je', 'TEST', 23, 'Jetest@test.fr', 'jesuislemdp'),
+('Kevin', 'PETIT', 23, 'mail@test.fr', 'jesuislemdp');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
